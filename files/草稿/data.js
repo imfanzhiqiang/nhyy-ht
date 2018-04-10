c1737f9420265709c93187844ed08f50e57461fe
imfanzhiqiang <imfanzhiqiang@gmail.com>
2018-04-10 16:54:52 +0800

    180410
    180410

---
+++ files/草稿/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,_(k,l),m,[n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,T),U,_(),V,_(),W,_(X,[])),Y,_(),Z,_());}; 
+var b="url",c="草稿.html",d="generationDate",e=new Date(1523350363396.17),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="sketchKeys",k="",l="s1",m="variables",n="OnLoadVariable",o="page",p="packageId",q="ca380d3682844d87873805293980bc53",r="type",s="Axure:Page",t="name",u="草稿",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFF2F2F2,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="55",O="colorStyle",P="grayscale",Q="fontName",R="Applied Font",S="borderWidth",T="0",U="adaptiveStyles",V="interactionMap",W="diagram",X="objects",Y="masters",Z="objectPaths";
+return _creator();
+})());
